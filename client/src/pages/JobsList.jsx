9acdbce22b4cdf33ff5b5d1591a2be15647b5664
Jean-Baptiste Benoist <jean_baptiste.benoist@epita.fr>
2019-10-11 17:46:04 +0200

    [JOB] Improve design

--- client/src/pages/JobsList.jsx
+++ client/src/pages/JobsList.jsx
@@ -8,7 +8,7 @@ import styled from 'styled-components'
 import 'react-table/react-table.css'
 
 const Wrapper = styled.div`
-    padding: 0 40px 40px 40px;
+    padding: 10px 40px 40px 40px;
 `
 /*
 const Update = styled.div`
