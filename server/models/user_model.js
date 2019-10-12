ea3f8f9f4d4258f0b2c11668955a3a1fd185d683
Brandon Quinne <brandon@luckeyhomes.com>
2019-10-12 21:01:54 +0200

    fix merging conflicts

--- server/models/user_model.js
+++ server/models/user_model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema(
@@ -8,7 +9,7 @@ const UserSchema = new Schema(
         description: { type: String},
         phone: { type: String},
         email: { type: String, unique: true, required: true },
-        passwordHash: { type: String, required: true },
+        password: { type: String, required: true },
         user_type: {
             type: String,
             enum: ['CANDIDATE', 'RECRUITER', 'ADMIN'],
@@ -27,4 +28,30 @@ const UserSchema = new Schema(
 );
 
 module.exports = mongoose.model('users', UserSchema);
+
+module.exports.createUser = function(newUser, callback) {
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(newUser.password, salt, function (err, hash) {
+            newUser.password = hash;
+            newUser.save(callback);
+        });
+    });
+};
+
+module.exports.comparePassword = function(candidatePassword, hash, callback){
+    bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
+        if(err) throw err;
+        callback(null, isMatch);
+    });
+};
+
+module.exports.getUserByEmail = function(email, callback){
+    var query = {email: email};
+    console.log('MODEL');
+    UserSchema.findOne(query, callback);
+};
+
+module.exports.getUserById = function(id, callback){
+    UserSchema.findById(id, callback);
+};
 
